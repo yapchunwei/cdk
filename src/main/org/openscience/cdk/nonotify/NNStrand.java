3bc5a6adf9472824ff5db227bc37d71f50a08333
Egon Willighagen <egonw@users.sourceforge.net>
2010-05-15 11:35:26 +0200

    One more class

--- src/main/org/openscience/cdk/nonotify/NNStrand.java
+++ src/main/org/openscience/cdk/nonotify/NNStrand.java
@@ -1,9 +1,4 @@
-/* $RCSfile$
- * $Author$
- * $Date$
- * $Revision$
- * 
- * Copyright (C) 2006-2007  Egon Willighagen <egonw@users.sf.net>
+/* Copyright (C) 2006-2007,2010  Egon Willighagen <egonw@users.sf.net>
  * 
  * Contact: cdk-devel@lists.sourceforge.net
  * 
@@ -27,20 +22,221 @@
  */
 package org.openscience.cdk.nonotify;
 
-import org.openscience.cdk.Strand;
+import java.util.Collection;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.interfaces.IMonomer;
+import org.openscience.cdk.interfaces.IStrand;
 
 /**
  * @cdk.module  nonotify
  * @cdk.githash
  */
-public class NNStrand extends Strand {
+public class NNStrand extends NNAtomContainer implements IStrand {
 
 	private static final long serialVersionUID = -1600146220587659667L;
 
-	public NNStrand () {
-		super();
-	}
+    /** The list of all Monomers in the Strand.*/
+    private Map<String, IMonomer> monomers;
+    /** The name of this strand (e.g. A, B). */
+    private String strandName;
+    /** The type of this strand (e.g. PEPTIDE, DNA, RNA). */
+    private String strandType;
+    
+    /**
+     * Constructs a new Strand.
+     */ 
+    public NNStrand() {
+        super();
+        // Stand stuff
+        monomers = new Hashtable<String, IMonomer>();
+        IMonomer oMonomer = getBuilder().newInstance(IMonomer.class);
+        oMonomer.setMonomerName("");
+        oMonomer.setMonomerType("UNKNOWN");
+        monomers.put("", oMonomer);
+                strandName = "";
+    }
+    
+    /**
+     * Retrieves the strand name.
+     *
+     * @return The name of the Strand object
+     * @see #setStrandName
+     */
+    public String getStrandName() {
+        return strandName;
+    }
+    
+    /**
+     * Retrieves the strand type.
+     *
+     * @return The type of the Strand object
+     * @see #setStrandType
+     */
+    public String getStrandType() {
+        return strandType;
+    }
+    
+    /**
+     * Sets the name of the Strand object.
+     *
+     * @param cStrandName  The new name for this strand
+     * @see #getStrandName
+     */
+    public void setStrandName(String cStrandName) {
+        strandName = cStrandName;
+    }
+    
+    /**
+     * Sets the type of the Strand object.
+     *
+     * @param cStrandType  The new type for this strand
+     * @see #getStrandType
+     */
+    public void setStrandType(String cStrandType) {
+        strandType = cStrandType;
+    }
+    
+    /**
+     *
+     * Adds the atom oAtom without specifying a Monomer or a Strand. Therefore the
+     * atom gets added to a Monomer of type UNKNOWN in a Strand of type UNKNOWN.
+     *
+     * @param oAtom  The atom to add
+     *
+     */
+    public void addAtom(IAtom oAtom) {
+        addAtom(oAtom, getMonomer(""));
+    }
+    
+    /**
+     *
+     * Adds the atom oAtom to a specific Monomer.
+     *
+     * @param oAtom  The atom to add
+     * @param oMonomer  The monomer the atom belongs to
+     *
+     */
+    public void addAtom(IAtom oAtom, IMonomer oMonomer) {
+        
+        int atomCount = super.getAtomCount();
+        
+        // Add atom to AtomContainer
+        super.addAtom(oAtom);
+
+        if(atomCount != super.getAtomCount()) { // ok, super did not yet contain the atom
+            
+            if (oMonomer == null) {
+                oMonomer = getMonomer("");
+            }
+            
+            oMonomer.addAtom(oAtom);
+            if (! monomers.containsKey(oMonomer.getMonomerName())) {
+                monomers.put(oMonomer.getMonomerName(), oMonomer);
+            }
+        }
+    }
+    
+    /**
+     *
+     * Returns the number of monomers present in the Strand.
+     *
+     * @return number of monomers
+     *
+     */
+    public int getMonomerCount() {
+        return monomers.size() - 1;
+    }
+    
+    /**
+     *
+     * Retrieves a Monomer object by specifying its name.
+     *
+     * @param cName  The name of the monomer to look for
+     * @return The Monomer object which was asked for
+     *
+     */
+    public IMonomer getMonomer(String cName) {
+        return monomers.get(cName);
+    }
+    
+    /**
+     * Returns a collection of the names of all <code>Monomer</code>s in this
+     * polymer.
+     *
+     * @return a <code>Collection</code> of all the monomer names.
+     */
+    public Collection<String> getMonomerNames() {
+        return monomers.keySet();
+    }
+    
+    /**
+     * 
+     * Adds a <code>Monomer</code> to this <code>Strand</code>. All atoms and
+     * bonds in the Monomer are added. NB: The <code>Monomer</code> will *not*
+     * "automatically" be connected to the <code>Strand</code>. That has to be
+     * done "manually" (as the "connection point" is not known). 
+     * @param monomer
+     */
+    /*public void addMonomer(Monomer monomer)   {
+        if (! monomers.contains(monomer.getMonomerName())) {
+            monomers.put(monomer.getMonomerName(), monomer);
+        }
+    }*/
+    
+    /**
+     * Removes a particular monomer, specified by its name.
+     * 
+     * @param name The name of the monomer to remove
+     */
+    public void removeMonomer(String name)  {
+        if (monomers.containsKey(name)) {
+            IMonomer monomer = (IMonomer)monomers.get(name);
+            this.remove(monomer);
+            monomers.remove(name);
+        }
+    }
+    
+    /**
+     * Returns a hashtable containing the monomers in the strand.
+     *
+     * @return hashtable containing the monomers in the strand.
+     */
+    public Map<String, IMonomer> getMonomers()  {
+        return monomers;
+    }
+    
+    public String toString() {
+        StringBuffer stringContent = new StringBuffer(32);
+        stringContent.append("Strand(");
+        stringContent.append(this.hashCode());
+        if (getStrandName() != null) {
+            stringContent.append(", N:").append(getStrandName());
+        }
+        if (getStrandType() != null) {
+            stringContent.append(", T:").append(getStrandType()).append(", ");
+        }
+        stringContent.append(super.toString());
+        stringContent.append(')');
+        return stringContent.toString();
+    }
+    
+    public Object clone() throws CloneNotSupportedException {
+        NNStrand clone = (NNStrand)super.clone();
+        clone.monomers.clear();
+        for (Iterator<String> iter = clone.getMonomerNames().iterator(); iter.hasNext();) {
+            IMonomer monomerClone = (IMonomer)(clone.getMonomer(iter.next().toString()).clone());
+            Iterator<IAtom> atoms = monomerClone.atoms().iterator();
+            while (atoms.hasNext()) {
+                clone.addAtom(atoms.next(), monomerClone);
+            }
+        }
+        return clone;
+    }
 	
 	public IChemObjectBuilder getBuilder() {
 		return NoNotificationChemObjectBuilder.getInstance();
